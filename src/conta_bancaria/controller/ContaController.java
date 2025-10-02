9ce624bd2350fa49e037e46374d91a42e963e6e4
nicollyjesus <nicolly.jesus.nj@gmail.com>
2025-10-02 14:22:06 -0300

    subindo a Atividade README 19.2

--- src/conta_bancaria/controller/ContaController.java
+++ src/conta_bancaria/controller/ContaController.java
@@ -2,19 +2,19 @@ package conta_bancaria.controller;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import conta_bancaria.model.Conta;
 import conta_bancaria.repository.ContaRepository;
 
 public class ContaController implements ContaRepository{
-
+	
 	private List<Conta> listaContas = new ArrayList<Conta>();
 	int numero = 0;
-	
+
 	@Override
 	public void listarTodas() {
-		
-		for(var conta: listaContas) {
+		for(var conta : listaContas) {
 			conta.visualizar();
 		}
 	}
@@ -22,32 +22,107 @@ public class ContaController implements ContaRepository{
 	@Override
 	public void cadastrar(Conta conta) {
 		listaContas.add(conta);
-		System.out.println("Conta cadastrada com sucesso!");
-		
+		System.out.println("\n🎉 Conta Cadastrada com sucesso!");
 	}
 
 	@Override
 	public void atualizar(Conta conta) {
-		// TODO Auto-generated method stub
+		
+		var buscarConta = buscarNaCollection(conta.getNumero());
+		
+		if(buscarConta != null) {
+			listaContas.set(listaContas.indexOf(buscarConta), conta);
+			System.out.printf("\nA Conta número: %d foi atualizada com sucesso!%n", conta.getNumero());
+		}else {
+			System.out.printf("\nA Conta número: %d não foi encontrada!%n", conta.getNumero());
+		}
 		
 	}
 
 	@Override
 	public void procurarPorNumero(int numero) {
-		// TODO Auto-generated method stub
+		
+		var conta = buscarNaCollection(numero);
+		
+		if(conta != null) {
+			conta.visualizar();
+		}else {
+			System.out.printf("\nA Conta número: %d não foi encontrada!%n", numero);
+		}
 		
 	}
 
 	@Override
 	public void deletar(int numero) {
-		// TODO Auto-generated method stub
+		
+		var conta = buscarNaCollection(numero);
+		
+		if(conta != null) {
+			if(listaContas.remove(conta) == true) {
+				System.out.printf("\nA conta número %d foi deletada com sucesso!%n", numero);
+			}
+		}else {
+			System.out.printf("\nA Conta número: %d não foi encontrada!%n", numero);
+		}
 		
 	}
 
 	@Override
 	public void sacar(int numero, float valor) {
-		// TODO Auto-generated method stub
+
+	    var conta = buscarNaCollection(numero);
+
+		if(conta != null) {
+			conta.depositar(valor);
+		System.out.printf("\nO depósito no valor de %.2f, na conta numero: %d foi efetuado com sucesso!%n", valor, numero);
+		}else {
+		System.out.printf("\nA Conta numero: %d nao foi encontrada!%n", numero);
+	}
+}
+
+	@Override
+	public void transferir(int numeroOrigem, int numeroDestino, float valor) {
 		
+		var contaOrigem = buscarNaCollection(numeroOrigem);
+		var contaDestino = buscarNaCollection(numeroDestino);
+		
+		if(contaOrigem != null && contaDestino != null) {
+			if(contaOrigem.sacar(valor) == true) {
+				contaDestino.depositar(valor);
+				System.out.println("/nA transferência no valor de %.2f, da conta número: %d para a conta: %d para a conta número: %d, foi efetuada com sucesso!%n");
+			}
+		} else {
+			System.out.println("\nA conta de Origem e/ou de Destino não foram encontradas!%n");
+		}
+		
+	}
+	
+	@Override
+    public void listarPorTitular(String titular) {
+        List<Conta> listaTitulares = listaContas.stream()
+                .filter(c -> c.getTitular().toUpperCase().contains(titular.toUpperCase()))
+                .collect(Collectors.toList());
+        if(listaTitulares.isEmpty()) {
+            System.out.printf("\nNenhuma conta foi encontrada para titulars que possuam o nome: %s", titular);
+        }
+        for(var conta: listaTitulares) {
+            conta.visualizar();
+        }
+    }
+		
+	// Métodos Auxiliares
+	public int gerarNumero() {
+		return ++ numero;
+	}
+	
+	public Conta buscarNaCollection(int numero) {
+		for(var conta : listaContas) {
+			if(conta.getNumero() == numero) {
+				return conta;
+			}
+		}
+		
+		return null;
 	}
 
 	@Override
@@ -55,13 +130,4 @@ public class ContaController implements ContaRepository{
 		// TODO Auto-generated method stub
 		
 	}
-
-	@Override
-	public void transferir(int numeroOrigem, int numeroDestino, float valor) {
-		// TODO Auto-generated method stub
-		
-	}
-
-
-	
 }
